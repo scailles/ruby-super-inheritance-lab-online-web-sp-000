65940ddb969de34e79c8339fc7233d8f0b50aeed
Shaina C <scailles23@gmail.com>
2021-01-10 07:11:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,12 @@ class Student
   
 
 def hello
-  "Hey there! I'm so excited to learn stuff."
+  puts "Hey there! I'm so excited to learn stuff."
 end
 
 
 def raise_hand
-  "Pick me!"
+  puts "Pick me!"
 end
   
   
